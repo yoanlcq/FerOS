bb01135e6bc9e51ccd4caf96589773b831dd26d3
yoanlcq <yoanlecoq.io@gmail.com>
2015-10-27 12:00:42 +0100

    Improved utils.h

--- include/kernel/utils.h
+++ include/kernel/utils.h
@@ -5,5 +5,34 @@
 
 int streq(const char *s1, const char *s2);
 int streqn(const char *s1, const char *s2, uint32_t n);
+strcat();
+strncat();
+strchr();
+strrchr();
+strchrnul();
+strcmp();
+strncmp();
+strcpy();
+strncpy();
+strdup();
+strfry();
+strlen();
+strstr();
+strtok();
+
+strtou64();
+strtos64();
+strtof64();
+rand();
+srand();
+
+malloc();
+realloc();
+free();
+abort();
+exit();
+bsearch();
+qsort();
+
 
 #endif
