7a85a6cd9009afe971e00069d75dcc6bd9da58f0
yoanlcq <yoanlecoq.io@gmail.com>
2015-10-28 09:27:35 +0100

    Edited node.h

--- include/kernel/node.h
+++ include/kernel/node.h
@@ -1,23 +1,45 @@
 #ifndef NODE_H
 #define NODE_H
 
-typedef int node;
+#include <stdint.h>
 
-node nopen(const uint8_t *url, uint32_t flags);
-int nclose(node n);
-int nflush(node n);
-int ngetc(node n, uint8_t *c);
-int ngets(node n, uint8_t *res, uint32_t length);
-int nprintf(node n, const uint8_t *fmt, ...);
-int nscanf(node n, const uint8_t *fmt, ...);
-int nputc(node n, uint8_t c);
-int nputs(node n, const uint8_t *str);
-int64_t nread(node n, uint8_t *str, int64_t nbytes);
-int64_t nwrite(node n, uint8_t *str, int64_t nbytes);
-int nseek(node n, int64_t offset, uint32_t whence);
-/* err can be NULL. */
-uint64_t ntell(node n, int *err);
+typedef void node;
+
+typedef enum nopen_flags {
+    N_READ     = 0x001,
+    N_WRITE    = 0x002,
+    N_RDWR     = 0x003,
+    N_EXEC     = 0x004,
+    N_APPEND   = 0x008,
+    N_TRUNC    = 0x010,
+    N_CREAT    = 0x020,
+    N_EXCL     = 0x040,
+    N_NOFOLLOW = 0x080,
+    N_NONBLOCK = 0x100,
+} nopen_flag;
+
+typedef enum nseek_whences {
+    NSEEK_SET  = 0,
+    NSEEK_CUR  = 1,
+    NSEEK_END  = 2,
+    NSEEK_DATA = 3,
+    NSEEK_HOLE = 4
+} nseek_whence;
+
+node *nopen(const uint8_t *url, uint32_t flags);
+int nclose(node* n);
+int nflush(node* n);
+uint8_t ngetc(node* n, int *err);
+uint64_t ngets(node* n, uint8_t *str, uint64_t nbytes, int *err);
+int nprintf(node* n, const uint8_t *fmt, ...);
+int nscanf(node* n, const uint8_t *fmt, ...);
+int nputc(node* n, uint8_t c);
+int nputs(node* n, const uint8_t *str);
+uint64_t nread(node* n, uint8_t *str, uint64_t nbytes, int *err);
+uint64_t nwrite(node* n, uint8_t *str, uint64_t nbytes, int *err);
+int nseek(node* n, int64_t offset, uint32_t whence);
+uint64_t ntell(node* n, int *err);
 int ndelete(const uint8_t *url);
-int nctl(node n, uint64_t op, ...);
+int nctl(node* n, uint64_t op, ...);
 
 #endif /* NODE_H */
