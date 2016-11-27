39bafbb171f82852446381a80fed82b4f18b706c
yoanlcq <yoanlecoq.io@gmail.com>
2016-11-27 18:29:57 +0100

    More tiny improvements

--- i686_boot/boot.c
+++ i686_boot/boot.c
@@ -1,20 +1,17 @@
 #define LFTAB "\n\t"
-#define asm __asm__
 #define SECTION(sec) __attribute__((section(#sec)))
-#define SECTOR_MEM 0x7C00
+#define SECTOR_MEM  0x7C00
 #define SECTOR_SIZE 512
-#define STACK_SIZE 4096
+#define STACK_SIZE  4096
 
 #include <stdnoreturn.h>
-
-const char hello[] = "Hello, Real Mode World!";
 
 static void putchar(char c) {
     asm (
         "mov al, %0"   LFTAB
         "mov ah, 0x0E" LFTAB
         "int 0x10"     LFTAB
-        : : "r"(c)
+        : : "q"(c)
     );
 }
 
@@ -27,9 +24,15 @@ static void puts(const char *str) {
 
 noreturn static void hang() { for(;;); }
 
+static void main(void) {
+    puts("Hello, Real Mode World!");
+    hang();
+}
+
 #define SECTOR_START ((SECTOR_MEM)/16)
 #define SECTOR_END   ((SECTOR_MEM+SECTOR_SIZE)/16)
 
+// Needs to be compiled with -fomit-frame-pointer
 SECTION(.start) void boot(void) {
     asm (
         "xor ax, ax"  LFTAB
@@ -41,6 +44,5 @@ SECTION(.start) void boot(void) {
         "mov esp, %2" LFTAB
         : : "i"(SECTOR_START), "i"(SECTOR_END), "i"(STACK_SIZE)
     );
-    puts(hello);
-    hang();
+    main();
 }
